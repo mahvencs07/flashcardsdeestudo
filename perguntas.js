bc4bafc6070691e61031099fd0c75f3cb0d8729f
mahvencs07 <00001093911219sp@al.educacao.sp.gov.br>
2024-11-03 19:06:17 -0300

    projetofinal

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'O primeiro presidente dos EUA foi George Washington.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é um ecossistema?',
+    'Um ecossistema é um sistema onde organismos interagem entre si e com o ambiente.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
